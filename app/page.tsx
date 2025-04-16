683c2744f4b71df4cfa695ad584c7589920ca81e
Noman-Asghar <157172872+Noman-Asghar@users.noreply.github.com>
2025-04-16 08:09:50 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,10 @@ import { FaHome } from "react-icons/fa";
 import Footer from "@/components/Footer";
 
 export default function Home() {
+   useEffect(() => {
+    // Client-side pe hi yeh code chalega
+    document.title = "My Page Title"; // Yeh line sirf client-side pe chalegi
+  }, []);
   return (
    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
     <div className="max-w-7xl w-full">
